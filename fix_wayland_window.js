84c8c57829b4b55b78f612891b33b21bfad82d03
shawnc722 <45184146+shawnc722@users.noreply.github.com>
2024-04-28 14:56:22 -0400

    add comments explaining the purpose of fix_wayland_window.js

--- fix_wayland_window.js
+++ fix_wayland_window.js
@@ -1,3 +1,6 @@
+// this is a KWin script that's only used in the specific case you're running KDE's Plasma desktop on Wayland.
+// if you are, it's loaded via dbus in widgets.py's start() function with the name 'pywidgets-fix'.
+// if a script with that name is already running, nothing else is done
 function setState(window) {
     if (window.caption == 'PyWidgets') {
         window.skipTaskbar = true;
